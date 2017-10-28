81406a2bc108971d0a86f67ac4cd6d8f0d0118d4
Paul Khomich <paulkhomich@outlook.com>
2017-10-28 18:39:31 +0500

    hope 90%

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -12,6 +12,28 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
+    let events = {};
+
+    function commandsMaker(command) {
+        let splits = command.split('.');
+        let commands = [splits[0] + '.'];
+        splits.forEach(function (el) {
+            if (splits.indexOf(el) !== 0) {
+                commands.push(commands[commands.length - 1] + el + '.');
+            }
+        });
+        commands.reverse();
+
+        return commands;
+    }
+
+    function commandsFinder(command) {
+        let keys = Object.keys(events);
+        let commands = keys.filter(key => key.startsWith(command + '.'));
+
+        return commands;
+    }
+
     return {
 
         /**
@@ -19,26 +41,54 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Params}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            event = event + '.';
+            if (!events[event]) {
+                events[event] = [];
+            }
+            events[event].push({ context, handler });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Params}
          */
         off: function (event, context) {
-            console.info(event, context);
+            let commands = commandsFinder(event);
+            commands.forEach(command => {
+                events[command].forEach(action => {
+                    if (action.context === context) {
+                        let index = events[command].indexOf(action);
+                        events[command].splice(index, 1);
+                    }
+                });
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Params}
          */
         emit: function (event) {
-            console.info(event);
+            let fullEvent = commandsMaker(event);
+            fullEvent.forEach(command => {
+                if (events[command]) {
+                    events[command].forEach(action => {
+                        action.handler.call(action.context);
+                    });
+                }
+            });
+
+            return this;
         },
 
         /**
